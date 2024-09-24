543aca4dadb7e4a00810211e1e4edd86be3da543
Bughalda <122581227+bughalda@users.noreply.github.com>
2024-09-23 21:44:16 -0600

    Update Mapa_farmacias.r

--- Primera Etapa/Mapa_farmacias.r
+++ Primera Etapa/Mapa_farmacias.r
@@ -4,7 +4,7 @@ library(dplyr)
 library(rnaturalearth)
 library(rnaturalearthdata)
 
-farmacias <- read.csv("farmacias_filtered.csv")
+farmacias <- read.csv("farmacias_categorizadas.csv")
 
 # Obtener los límites de los estados de México usando rnaturalearth
 mexico_estados <- ne_states(country = "Mexico", returnclass = "sf")
@@ -12,16 +12,22 @@ mexico_estados <- ne_states(country = "Mexico", returnclass = "sf")
 # Convertir los datos de farmacias a un objeto sf
 farmacias_sf <- st_as_sf(farmacias, coords = c("longitud", "latitud"), crs = 4326, agr = "constant")
 
+# Filtrar solo farmacias grandes y medianas
+farmacias_sf <- farmacias_sf %>%
+  filter(categoria != "Small")
+
 # Crear el mapa interactivo con clusters de puntos
 mapa <- leaflet(farmacias_sf) %>%
   addTiles() %>%
   # Añadir los puntos con la funcionalidad de clusterización
   addCircleMarkers(
     radius = 5,  # Tamaño fijo para los círculos
-    color = "blue",
+    color = ~ifelse(categoria == "Grande", "red", ifelse(categoria == "Mediana", "orange", "blue")),
     stroke = FALSE, 
     fillOpacity = 0.6,
-    popup = ~paste("Farmacia:", raz_social),
+    popup = ~paste("Establecimiento:", nom_estab, "<br>",
+                   "Código Postal:", cod_postal, "<br>",
+                   "Categoría:", categoria),
     clusterOptions = markerClusterOptions()  # Habilitar los clusters de puntos
   ) %>%
   addPolygons(data = mexico_estados,  # Añadir los límites de los estados
